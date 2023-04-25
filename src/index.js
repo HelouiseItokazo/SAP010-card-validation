1747b4a0237fb24dcb8bcf726cb6d86e4bffc13f
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-04-25 18:23:53 -0300

    first-commit-initial-project-structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
 
-console.log(validator);
+document.getElementById("inputCardNumber").addEventListener("keyup", () => {
+  const cardNumber = document.getElementById("inputCardNumber");
+  printMaskify(validator.maskify(cardNumber.value));
+});
+
+document.getElementById("submit").addEventListener("click", () => {
+  const cardNumber = document.getElementById("inputCardNumber").value;
+  printValidationStatus(validator.isValid(cardNumber.toString()));
+});
+
+function printValidationStatus(cardNumberIsValid) {
+  cardNumberIsValid ? document.getElementById("cardNumberValidationStatus").innerHTML = "Número do cartão validado com sucesso!" :
+    document.getElementById("cardNumberValidationStatus").innerHTML = "Número do cartão invalido!";
+}
+
+function printMaskify(creditCardNumber){
+  document.getElementById("cardShowNumber").innerHTML = creditCardNumber;
+}
+
+
+
