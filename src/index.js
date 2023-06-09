eee310138475eb95cc8a75f6aaf399ba73769d2e
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-06-09 17:20:59 -0300

    mudança na estilização

--- src/index.js
+++ src/index.js
@@ -1,23 +1,56 @@
 import validator from './validator.js';
 
-document.getElementById("inputCardNumber").addEventListener("keyup", () => {
-  const cardNumber = document.getElementById("inputCardNumber");
-  printMaskify(validator.maskify(cardNumber.value));
-});
+const nameOnCardInput = document.querySelector('.o-card-holder-form');
 
-document.getElementById("submit").addEventListener("click", () => {
-  const cardNumber = document.getElementById("inputCardNumber").value;
-  printValidationStatus(validator.isValid(cardNumber.toString()));
-});
-
-function printValidationStatus(cardNumberIsValid) {
-  cardNumberIsValid ? document.getElementById("cardNumberValidationStatus").innerHTML = "Número do cartão validado com sucesso!" :
-    document.getElementById("cardNumberValidationStatus").innerHTML = "Número do cartão invalido!";
+if(nameOnCardInput){
+  nameOnCardInput.addEventListener('input', () => {
+    const cardHolder = document.querySelector('.o-name-on-card');
+    cardHolder.innerHTML = nameOnCardInput.value.toUpperCase();
+    const userIcon = document.querySelector('.o-face-form');
+    userIcon.style.color = '#67b8bd';
+    if(nameOnCardInput.value === ''){
+      cardHolder.innerHTML = 'Name on card';
+      userIcon.style.color = '';
+    }
+  })
 }
 
-function printMaskify(creditCardNumber){
-  document.getElementById("cardShowNumber").innerHTML = creditCardNumber;
+const numberCardInput = document.querySelector('.o-card-number-form');
+if(numberCardInput){
+  numberCardInput.addEventListener('input', () => {
+    const numberCard = document.querySelector('.o-card-number');
+    const cardIcon = document.querySelector('.o-card-form');
+    cardIcon.style.color = '#67b8bd'
+    const maskedNumber = validator.maskify(numberCardInput.value);
+    numberCard.innerHTML = maskedNumber;
+    if(numberCardInput.value === ''){
+      numberCard.innerHTML = 'XXXX XXXX XXXX XXXX';
+      cardIcon.style = '';
+    }
+  })
 }
 
+const expiryDate = document.querySelector('.o-mm-yy-form');
+if(expiryDate){
+  expiryDate.addEventListener('input', ()=>{
+    const expCard = document.querySelector('.o-mm-yy');
+    const calendarIcon = document.querySelector('.o-calendar-form');
+    calendarIcon.style.color = '#67b8bd';
+    expCard.innerHTML = expiryDate.value;
+    if(expiryDate.value == ''){
+      expCard.innerHTML = 'MM/YY';
+      calendarIcon.style = '';
+    }
+  })
+}
 
-
+const cvv = document.querySelector('.o-input-cvv-form');
+if(cvv){
+  cvv.addEventListener('input', () => {
+    const lockIcon = document.querySelector('.o-lock-form');
+    lockIcon.style.color = '#67b8bd';
+    if(cvv.value ==  ''){
+      lockIcon.style.color = '';
+    }
+  })
+}
--- src/validator.js
+++ src/validator.js
@@ -8,56 +8,50 @@ const validator = {
 
     const validatedCardNumbers = regexOnlyNumbersNoBlanks.test(inputCardNumber);
 
-    if (validatedCardNumbers) {
-
-      let cardNumber = inputCardNumber.split('').reverse();
+    let cardNumber = inputCardNumber.split('').reverse();
 
 
-      cardNumber = cardNumber.map(function (item) {
-        return parseInt(item, 10);
-      });
+    cardNumber = cardNumber.map(function (item) {
+      return parseInt(item, 10);
+    });
 
 
-      const finalIndex = cardNumber.length - 1;
-      let sumOfAllDigits = 0;
+    const finalIndex = cardNumber.length - 1;
+    let sumOfAllDigits = 0;
 
 
-      for (let index = 0; index <= finalIndex; index++) {
+    for (let index = 0; index <= finalIndex; index++) {
 
 
-        if (index % 2 !== 0) {
-          cardNumber[index] = cardNumber[index] * 2;
+      if (index % 2 !== 0) {
+        cardNumber[index] = cardNumber[index] * 2;
 
 
-          if (cardNumber[index] > 9) {
-            const digits = cardNumber[index].toString(10).split('');
-            const firstDigit = parseInt(digits.slice(0, 1));
-            const secondtDigit = parseInt(digits.slice(1));
-            const sumOfDigits = firstDigit + secondtDigit;
-            cardNumber[index] = sumOfDigits;
-          }//endIf
-
+        if (cardNumber[index] > 9) {
+          const digits = cardNumber[index].toString(10).split('');
+          const firstDigit = parseInt(digits.slice(0, 1));
+          const secondtDigit = parseInt(digits.slice(1));
+          const sumOfDigits = firstDigit + secondtDigit;
+          cardNumber[index] = sumOfDigits;
         }//endIf
 
-      }//endFor
+      }//endIf
 
-      for (let index = 0; index <= finalIndex; index++) {
-        sumOfAllDigits += cardNumber[index];
-      }//endFor
+    }//endFor
+
+    for (let index = 0; index <= finalIndex; index++) {
+      sumOfAllDigits += cardNumber[index];
+    }//endFor
 
 
-      const digits = sumOfAllDigits.toString(10).split('');
-      const lastDigit = parseInt(digits.slice(1));
+    const digits = sumOfAllDigits.toString(10).split('');
+    const lastDigit = parseInt(digits.slice(1));
 
-      if(lastDigit === 0){
-        return true;
-      } else {
-        return false;
-      }
-
+    if (lastDigit === 0 || inputCardNumber === '')  {
+      return true;
     } else {
-      return alert("Digite apenas números");
-    }//endIf
+      return false;
+    }
 
   },//endIsValid
 
@@ -73,14 +67,24 @@ const validator = {
 
     const maskedNumber = digitsThatWillBeMasked.replace(digitsThatWillBeMasked, () => {
       for (let i = 0; i < digitsThatWillBeMasked.length; i++) {
-        digitsThatWillBeMasked = digitsThatWillBeMasked.replace(digitsThatWillBeMasked[i], "#");
+        digitsThatWillBeMasked = digitsThatWillBeMasked.replace(digitsThatWillBeMasked[i], '*');
       }
       return digitsThatWillBeMasked;
     });
 
-    const concatenated = maskedNumber + lastFourIndexes;
+    let concatenated = maskedNumber + lastFourIndexes;
+    let formated = '';
 
-    return concatenated;
+    for (let i = 0; i < concatenated.length; i++) {
+      if (i % 4 === 0) {
+        formated += ' ';
+        formated += concatenated[i]
+      } else {
+        formated += concatenated[i];
+      }
+    }
+
+    return formated;
 
   },//endMaskify
 
